558d10c693bb45a803466598a64802618aa03002
uripper <36463273+uripper@users.noreply.github.com>
2023-05-26 04:03:06 -0600

    The changes made in this commit include: - Modified .gitignore to ignore all files in node_modules directory - Modified portfolio/src/lava_lamp.ts to pass p5 instance as an argument to Blob class constructor, and modified Blob class methods update() and display() to use the passed p5 instance instead of p5 global variable. - Added a script tag in portfolio/templates/work.html to load lava_lamp.js file.

--- portfolio/src/lava_lamp.ts
+++ portfolio/src/lava_lamp.ts
@@ -8,36 +8,35 @@ class Blob {
   speedY: number;
   color: p5.Color;
 
-  constructor() {
-    this.x = Math.random() * p5.windowWidth;
-    this.y = Math.random() * p5.windowHeight;
+  constructor(p: p5) {
+    this.x = Math.random() * p.windowWidth;
+    this.y = Math.random() * p.windowHeight;
     this.radius = Math.random() * 100 + 50;
     this.speedX = Math.random() * 4 - 2;
     this.speedY = Math.random() * 4 - 2;
-    this.color = new p5.Color(
-      Math.random() * 255,
-      Math.random() * 255,
-      Math.random() * 255,
-      100
-    );
+    this.color = new p5.Color();
+    this.color.setBlue(Math.random() * 255);
+    this.color.setGreen(Math.random() * 255);
+    this.color.setRed(Math.random() * 255);
+    this.color.setAlpha(100);
   }
 
-  update() {
+  update(p: p5) {
     this.x += this.speedX;
     this.y += this.speedY;
 
-    if (this.x - this.radius < 0 || this.x + this.radius > p5.windowWidth) {
+    if (this.x - this.radius < 0 || this.x + this.radius > p.windowWidth) {
       this.speedX *= -1;
     }
-    if (this.y - this.radius < 0 || this.y + this.radius > p5.windowHeight) {
+    if (this.y - this.radius < 0 || this.y + this.radius > p.windowHeight) {
       this.speedY *= -1;
     }
   }
 
-  display() {
-    p5.noStroke();
-    p5.fill(this.color);
-    p5.ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
+  display(p: p5) {
+    p.noStroke();
+    p.fill(this.color);
+    p.ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
   }
 }
 
@@ -47,15 +46,15 @@ const sketch = (p: p5) => {
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
     for (let i = 0; i < 10; i++) {
-      blobs.push(new Blob());
+      blobs.push(new Blob(p));
     }
   };
 
   p.draw = () => {
     p.background(0);
     for (let blob of blobs) {
-      blob.update();
-      blob.display();
+      blob.update(p);
+      blob.display(p);
     }
   };
 };
