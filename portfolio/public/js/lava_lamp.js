4a8f9e73204968ce2939c0ae51850b0e78b96115
uripper <36463273+uripper@users.noreply.github.com>
2023-05-28 15:16:50 -0600

    added a lot of functions

--- portfolio/public/js/lava_lamp.js
+++ portfolio/public/js/lava_lamp.js
@@ -1,49 +1,119 @@
-import p5 from "p5";
-export class Blob {
-    constructor(p) {
-        this.x = Math.random() * p.windowWidth;
-        this.y = Math.random() * p.windowHeight;
-        this.radius = Math.random() * 100 + 50;
-        this.speedX = Math.random() * 4 - 2;
-        this.speedY = Math.random() * 4 - 2;
-        this.color = new p5.Color();
-        this.color.setBlue(Math.random() * 255);
-        this.color.setGreen(Math.random() * 255);
-        this.color.setRed(Math.random() * 255);
-        this.color.setAlpha(100);
-    }
-    update(p) {
-        this.x += this.speedX;
-        this.y += this.speedY;
-        if (this.x - this.radius < 0 || this.x + this.radius > p.windowWidth) {
-            this.speedX *= -1;
+var color_palette = [
+    "#141414",
+    "#000000",
+    "#ffc8f0",
+    "#f0ffa0",
+    "#c8dcff",
+    "#f0c8ff",
+];
+
+window.onload = function() {
+    new p5(function(p) {
+        let abstract_shapeGenerator;
+
+        p.setup = function() {
+            let canvas = p.createCanvas(p.windowWidth, p.windowHeight);
+            canvas.parent('background');
+
+            abstract_shapeGenerator = createNewShape();
+        };
+
+        p.draw = function() {
+            p.background(0, 10); // Add some transparency to create fading effect
+
+            // Slowly shift and change the abstract shape
+            if (p.frameCount % 300 === 0) {
+                abstract_shapeGenerator.shiftShape();
+                abstract_shapeGenerator.changeShape();
+            }
+
+            abstract_shapeGenerator.move();
+            abstract_shapeGenerator.show();
+
+            // If the shape is out of bounds, create a new one
+            if (abstract_shapeGenerator.y < -abstract_shapeGenerator.r) {
+                abstract_shapeGenerator = createNewShape();
+            }
+        };
+
+        function createNewShape() {
+            let x = p.width / 2;
+            let y = p.height;
+            let r = p.random(100, 200);
+            let color = p.random(color_palette);
+            return new AbstractShape(x, y, r, color);
         }
-        if (this.y - this.radius < 0 || this.y + this.radius > p.windowHeight) {
-            this.speedY *= -1;
+
+        p.windowResized = function() {
+            p.resizeCanvas(p.windowWidth, p.windowHeight);
+        };
+
+        class AbstractShape {
+            constructor(x, y, r, color) {
+                this.x = x;
+                this.y = y;
+                this.r = r;
+                this.color = p.color(color);
+                this.numPoints = p.floor(p.random(5, 10)); // Random number of control points for the curve
+                this.controlPoints = [];
+                for (let i = 0; i < this.numPoints; i++) {
+                    let controlX = p.random(-this.r, this.r);
+                    let controlY = p.random(-this.r, this.r);
+                    this.controlPoints.push({ x: controlX, y: controlY });
+                }
+                this.targetColor = p.color(p.random(color_palette));
+                this.targetPoints = this.controlPoints.map(point => ({ ...point }));
+            }
+
+            shiftShape() {
+                this.targetPoints = [];
+                for (let i = 0; i < this.numPoints; i++) {
+                    let controlX = p.random(-this.r, this.r);
+                    let controlY = p.random(-this.r, this.r);
+                    this.targetPoints.push({ x: controlX, y: controlY });
+                }
+            }
+
+            changeShape() {
+                this.targetColor = p.color(p.random(color_palette));
+            }
+
+            move() {
+                this.y -= 0.5;
+                this.x += p.sin(p.frameCount / 300) * 0.5;
+
+                // Interpolate controlPoints and color
+                for (let i = 0; i < this.numPoints; i++) {
+                    this.controlPoints[i].x = p.lerp(this.controlPoints[i].x, this.targetPoints[i].x, 0.01);
+                    this.controlPoints[i].y = p.lerp(this.controlPoints[i].y, this.targetPoints[i].y, 0.01);
+                }
+                this.color = p.lerpColor(this.color, this.targetColor, 0.01);
+            }
+
+            show() {
+                let gradientSteps = 10;
+                p.noStroke();
+
+                for (let i = 0; i < gradientSteps; i++) {
+                    let ratio = i / gradientSteps;
+                    let colorAlpha = p.lerpColor(this.color, this.targetColor, ratio);
+                    colorAlpha.setAlpha((1 - ratio) * 255);
+                    p.fill(colorAlpha);
+                    drawWackyCurve(this.x, this.y, this.r * (1 - ratio), this.numPoints, this.controlPoints);
+                }
+            }
         }
-    }
-    display(p) {
-        p.noStroke();
-        p.fill(this.color);
-        p.ellipse(this.x, this.y, this.radius * 2, this.radius * 2);
-    }
-}
-const sketch = (p) => {
-    let blobs = [];
-    p.setup = () => {
-        p.createCanvas(p.windowWidth, p.windowHeight);
-        for (let i = 0; i < 10; i++) {
-            blobs.push(new Blob(p));
+
+        function drawWackyCurve(x, y, radius, numPoints, controlPoints) {
+            p.beginShape();
+            let angleStep = p.TWO_PI / numPoints;
+            for (let i = 0; i < numPoints; i++) {
+                let angle = i * angleStep;
+                let px = x + radius * p.cos(angle) + controlPoints[i].x;
+                let py = y + radius * p.sin(angle) + controlPoints[i].y;
+                p.curveVertex(px, py);
+            }
+            p.endShape(p.CLOSE);
         }
-    };
-    p.draw = () => {
-        p.background(0);
-        for (let blob of blobs) {
-            blob.update(p);
-            blob.display(p);
-        }
-    };
+    });
 };
-// Create a new p5 instance
-new p5(sketch);
-//# sourceMappingURL=lava_lamp.js.map
