14fa1fec8f4e5df19c1bd0d9b0a01352813e2141
uripper <36463273+uripper@users.noreply.github.com>
2023-05-29 17:42:39 -0600

    The changes made in this commit include: - Modified portfolio/public/homepages.css to change the background color, text color, header color, accent color, and shadow color. - Modified portfolio/sass/homepages.scss to remove unnecessary code and modify the font family for the header. - Modified portfolio/static/js/lava_lamp.js to change the cell size, add variables for columns, rows, grid, nextGrid, frameRate and cellOpacity. Also added functions createRandomGrid(), createEmptyGrid(), updateGrid() and countNeighbors().

--- portfolio/public/js/lava_lamp.js
+++ portfolio/public/js/lava_lamp.js
@@ -1,159 +1,122 @@
 var color_palette = [
-    "#141414",
-    "#000000",
-    "#ffc8f0",
-    "#f0ffa0",
-    "#c8dcff",
-    "#f0c8ff",
-    "#9A83C9",
-    "#9D1A1C",
-    "#F1493B",
-    "#84231D",
-    "#FFEC3F",
-
+    "#0056C6",
+    "#0E1452",
+    "#1B78E1",
+    "#1D1C2F",
+    "#0072E4",
+    "#2E73FF",
+    "#40D1FF",
+    "#4AB7FF",
+    "#515180",
+    "#5FE5FF",
+    "#253791",
+    "#49B7FF",
+    "#3B64EA",
 ];
 
 window.onload = function() {
     new p5(function(p) {
-        let abstract_shapeGenerator;
+        var cellSize = 10;
+        var columns, rows;
+        var grid, nextGrid;
+        var frameRate = 10; // Adjust this value to control the speed
+        var cellOpacity = 100; // Adjust this value to control the opacity
 
         p.setup = function() {
             let canvas = p.createCanvas(p.windowWidth, p.windowHeight);
             canvas.parent('background');
-            p.noiseDetail(8, 0.65);
 
+            columns = p.floor(p.width / cellSize);
+            rows = p.floor(p.height / cellSize);
 
-            abstract_shapeGenerator = createNewShape();
+            grid = createRandomGrid();
+            nextGrid = createEmptyGrid();
+
+            p.frameRate(frameRate);
         };
-        var gradient_colors = [
-            "#0056C6",
-            "#0E1452",
-            "#1B78E1",
-            "#1D1C2F",
-            "#0072E4",
-            "#2E73FF",
-            "#40D1FF",
-            "#4AB7FF",
-            "#515180",
-            "#5FE5FF",
-            "#253791",
-            "#49B7FF",
-            "#3B64EA",
-        ];
-        
+
         p.draw = function() {
             p.background(0);
-        
-            // Draw Perlin noise background
-            for (let y = 0; y < p.height; y++) {
-                let noiseVal = p.noise(y * 0.001, p.frameCount * 0.0005);
-                let colorIndex = Math.floor(noiseVal * gradient_colors.length);
-                colorIndex = p.constrain(colorIndex, 0, gradient_colors.length - 1);
-        
-                let colorStart = p.color(gradient_colors[colorIndex] + '80');
-                let colorEnd = p.color(gradient_colors[(colorIndex + 1) % gradient_colors.length] + '80');
-                let lerpVal = (noiseVal * gradient_colors.length) - colorIndex;
-        
-                let c = p.lerpColor(colorStart, colorEnd, lerpVal);
-        
-                p.stroke(c);
-                p.line(0, y, p.width, y);
+
+            for (let i = 0; i < columns; i++) {
+                for (let j = 0; j < rows; j++) {
+                    let x = i * cellSize;
+                    let y = j * cellSize;
+
+                    if (grid[i][j] === 1) {
+                        let color = p.random(color_palette) + p.hex(cellOpacity, 2); // Set alpha value for opacity
+                        p.fill(color);
+                        p.noStroke();
+                        p.rect(x, y, cellSize, cellSize);
+                    }
+                }
             }
 
-        
-            // Slowly shift and change the abstract shape
-            if (p.frameCount % 300 === 0) {
-                abstract_shapeGenerator.shiftShape();
-                abstract_shapeGenerator.changeShape();
-            }
-        
-            abstract_shapeGenerator.move();
-            abstract_shapeGenerator.show();
-        
-            // If the shape is out of bounds, create a new one
-            if (abstract_shapeGenerator.y < -abstract_shapeGenerator.r) {
-                abstract_shapeGenerator = createNewShape();
-            }
+            updateGrid();
         };
-        
-        function createNewShape() {
-            let x = p.width / 2;
-            let y = p.height;
-            let r = p.random(100, 200);
-            let color = p.random(color_palette);
-            return new AbstractShape(x, y, r, color);
+
+        function createRandomGrid() {
+            let grid = new Array(columns);
+            for (let i = 0; i < columns; i++) {
+                grid[i] = new Array(rows);
+                for (let j = 0; j < rows; j++) {
+                    grid[i][j] = p.floor(p.random(2));
+                }
+            }
+            return grid;
+        }
+
+        function createEmptyGrid() {
+            let grid = new Array(columns);
+            for (let i = 0; i < columns; i++) {
+                grid[i] = new Array(rows);
+                for (let j = 0; j < rows; j++) {
+                    grid[i][j] = 0;
+                }
+            }
+            return grid;
+        }
+
+        function updateGrid() {
+            for (let i = 0; i < columns; i++) {
+                for (let j = 0; j < rows; j++) {
+                    let state = grid[i][j];
+                    let neighbors = countNeighbors(grid, i, j);
+
+                    if (state === 0 && neighbors === 3) {
+                        nextGrid[i][j] = 1;
+                    } else if (state === 1 && (neighbors < 2 || neighbors > 3)) {
+                        nextGrid[i][j] = 0;
+                    } else {
+                        nextGrid[i][j] = state;
+                    }
+                }
+            }
+
+            let temp = grid;
+            grid = nextGrid;
+            nextGrid = temp;
+        }
+
+        function countNeighbors(grid, x, y) {
+            let sum = 0;
+            for (let i = -1; i <= 1; i++) {
+                for (let j = -1; j <= 1; j++) {
+                    let col = (x + i + columns) % columns;
+                    let row = (y + j + rows) % rows;
+                    sum += grid[col][row];
+                }
+            }
+            sum -= grid[x][y];
+            return sum;
         }
 
         p.windowResized = function() {
             p.resizeCanvas(p.windowWidth, p.windowHeight);
+            columns = p.floor(p.width / cellSize);
+            rows = p.floor(p.height / cellSize);
+            grid = createRandomGrid();
+            nextGrid = createEmptyGrid();
         };
-
-        class AbstractShape {
-            constructor(x, y, r, color) {
-                this.x = x;
-                this.y = y;
-                this.r = r;
-                this.color = p.color(color);
-                this.numPoints = p.floor(p.random(5, 10)); // Random number of control points for the curve
-                this.controlPoints = [];
-                for (let i = 0; i < this.numPoints; i++) {
-                    let controlX = p.random(-this.r, this.r);
-                    let controlY = p.random(-this.r, this.r);
-                    this.controlPoints.push({ x: controlX, y: controlY });
-                }
-                this.targetColor = p.color(p.random(color_palette));
-                this.targetPoints = this.controlPoints.map(point => ({ ...point }));
-            }
-
-            shiftShape() {
-                this.targetPoints = [];
-                for (let i = 0; i < this.numPoints; i++) {
-                    let controlX = p.random(-this.r, this.r);
-                    let controlY = p.random(-this.r, this.r);
-                    this.targetPoints.push({ x: controlX, y: controlY });
-                }
-            }
-
-            changeShape() {
-                this.targetColor = p.color(p.random(color_palette));
-            }
-
-            move() {
-                this.y -= 0.5;
-                this.x += p.sin(p.frameCount / 300) * 0.5;
-
-                // Interpolate controlPoints and color
-                for (let i = 0; i < this.numPoints; i++) {
-                    this.controlPoints[i].x = p.lerp(this.controlPoints[i].x, this.targetPoints[i].x, 0.01);
-                    this.controlPoints[i].y = p.lerp(this.controlPoints[i].y, this.targetPoints[i].y, 0.01);
-                }
-                this.color = p.lerpColor(this.color, this.targetColor, 0.01);
-            }
-
-            show() {
-                let gradientSteps = 10;
-                p.noStroke();
-
-                for (let i = 0; i < gradientSteps; i++) {
-                    let ratio = i / gradientSteps;
-                    let colorAlpha = p.lerpColor(this.color, this.targetColor, ratio);
-                    colorAlpha.setAlpha((1 - ratio) * 255);
-                    p.fill(colorAlpha);
-                    drawWackyCurve(this.x, this.y, this.r * (1 - ratio), this.numPoints, this.controlPoints);
-                }
-            }
-        }
-
-        function drawWackyCurve(x, y, radius, numPoints, controlPoints) {
-            p.beginShape();
-            let angleStep = p.TWO_PI / numPoints;
-            for (let i = 0; i < numPoints; i++) {
-                let angle = i * angleStep;
-                let px = x + radius * p.cos(angle) + controlPoints[i].x;
-                let py = y + radius * p.sin(angle) + controlPoints[i].y;
-                p.curveVertex(px, py);
-            }
-            p.endShape(p.CLOSE);
-        }
     });
 };
