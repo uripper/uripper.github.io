a0b7294921f624efa44d0f937f378738e2c24e96
uripper <36463273+uripper@users.noreply.github.com>
2023-06-04 20:16:31 -0600

    changes

--- js/button.js
+++ js/button.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         fadeToBlack.style.zIndex = "9999";
         fadeToBlack.style.opacity = "100%";
         setTimeout(function() {
-            open("/home");
+            location.href = "/home";
         }, 1000); // Delay the page navigation by 3 seconds
     });
     button.addEventListener("touchstart", function( event ) {   
