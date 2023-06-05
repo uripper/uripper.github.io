10d051f6826cb442b27f18c0ac1d45f165a2613b
uripper <36463273+uripper@users.noreply.github.com>
2023-06-04 19:53:13 -0600

    changing game of life to a different name since it's not really the game of life anymore

--- js/game_of_likelifekinda.js
+++ js/game_of_likelifekinda.js
@@ -1,26 +1,22 @@
 var color_palette = [
-    "#0056C6",
-    "#0E1452",
-    "#1B78E1",
-    "#1D1C2F",
-    "#0072E4",
-    "#2E73FF",
-    "#40D1FF",
-    "#4AB7FF",
-    "#515180",
-    "#5FE5FF",
-    "#253791",
-    "#49B7FF",
-    "#3B64EA",
+    "#0056C6", "#0E1452", "#1B78E1", "#1D1C2F", "#0072E4", "#2E73FF", "#40D1FF", 
+    "#4AB7FF", "#515180", "#5FE5FF", "#253791", "#49B7FF", "#3B64EA",
 ];
+
+let rgbColorPalette = color_palette.map(color => {
+    let r = parseInt(color.substring(1, 3), 16);
+    let g = parseInt(color.substring(3, 5), 16);
+    let b = parseInt(color.substring(5, 7), 16);
+    return { r, g, b };
+});
 
 window.onload = function() {
     new p5(function(p) {
         var cellSize = 5;
         var columns, rows;
-        var grid, nextGrid;
+        var grid;
         var framerateValue = 60;
-        var cellOpacity = 100;
+        var cellOpacity = 65;
 
         p.setup = function() {
             let canvas = p.createCanvas(p.windowWidth, p.windowHeight);
@@ -30,7 +26,6 @@ window.onload = function() {
             rows = p.floor(p.height / cellSize);
 
             grid = createRandomGrid();
-            nextGrid = createEmptyGrid();
 
             p.frameRate(framerateValue);
         };
@@ -43,13 +38,9 @@ window.onload = function() {
                     let x = i * cellSize;
                     let y = j * cellSize;
 
-                    if (grid[i][j] === 1) {
-                        let color = p.random(color_palette);
-                        color = color.substring(1); // remove #
-                        let r = parseInt(color.substring(0, 2), 16);
-                        let g = parseInt(color.substring(2, 4), 16);
-                        let b = parseInt(color.substring(4, 6), 16);
-                        p.fill(r, g, b, cellOpacity);
+                    if (grid[i][j].state === 1) {
+                        let color = grid[i][j].color;
+                        p.fill(color.r, color.g, color.b, cellOpacity);
                         p.noStroke();
                         p.rect(x, y, cellSize, cellSize);
                     }
@@ -61,13 +52,14 @@ window.onload = function() {
 
         function createRandomGrid() {
             return Array.from({ length: columns }, () =>
-                Array.from({ length: rows }, () => p.floor(p.random(2)))
+                Array.from({ length: rows }, () => {
+                    let state = p.floor(p.random(2));
+                    let color = rgbColorPalette[p.floor(p.random(rgbColorPalette.length))];
+                    return { state, color };
+                })
             );
         }
 
-        function createEmptyGrid() {
-            return Array.from({ length: columns }, () => Array.from({ length: rows }, () => 0));
-        }
         let lastLivingCells = 0;
         let livingCells = 0;
         let sameStateCount= 0;
@@ -81,49 +73,42 @@ window.onload = function() {
             let max = 3;
             for (let i = 0; i < columns; i++) {
                 for (let j = 0; j < rows; j++) {
-                    let state = grid[i][j];
-                    let neighbors = Math.round(countNeighbors(grid, i, j));  
+                    let cell = grid[i][j];
+                    let neighbors = countNeighbors(i, j);  
 
-                    if (state === dead && neighbors === max) {
-                        nextGrid[i][j] = alive;
+                    if (cell.state === dead && neighbors === max) {
+                        cell.state = alive;
+                        cell.color = rgbColorPalette[p.floor(p.random(rgbColorPalette.length))];
                         livingCells++;
-                    } else if (state === alive && (neighbors < min || neighbors > max)) {
-                        nextGrid[i][j] = dead;
+                    } else if (cell.state === alive && (neighbors < min || neighbors > max)) {
+                        cell.state = dead;
                         livingCells++;
-                    } else {
-                        nextGrid[i][j] = state;
-                        if (state === alive) {
-                            livingCells++;
-                        }
                     }
                 }
             }
 
-            if (livingCells == lastLivingCells) {
+            if (livingCells === lastLivingCells) {
                 sameStateCount++;
-                [grid, nextGrid] = [nextGrid, grid];
-                if(sameStateCount >= sameStateTolerance){
+                if (sameStateCount >= sameStateTolerance) {
                     grid = createRandomGrid();
-                    nextGrid = createEmptyGrid();
                     sameStateCount = 0;
                 }
             } else {
-                [grid, nextGrid] = [nextGrid, grid];
                 lastLivingCells = livingCells;
                 sameStateCount = 0;
             }
         }
 
-        function countNeighbors(grid, x, y) {
+        function countNeighbors(x, y) {
             let sum = 0;
             for (let i = -1; i <= 1; i++) {
                 for (let j = -1; j <= 1; j++) {
                     let col = (x + i + columns) % columns;
                     let row = (y + j + rows) % rows;
-                    sum += grid[col][row];
+                    sum += grid[col][row].state;
                 }
             }
-            sum -= grid[x][y];
+            sum -= grid[x][y].state;
             return sum;
         }
 
@@ -132,7 +117,6 @@ window.onload = function() {
             columns = p.floor(p.width / cellSize);
             rows = p.floor(p.height / cellSize);
             grid = createRandomGrid();
-            nextGrid = createEmptyGrid();
         };
     });
 };
